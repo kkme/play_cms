70410660eb741ba68c1e8089b60857b52fd36ea2
thaigialai1987 <thaigialai1987@gmail.com>
2014-05-31 16:46:13 +0700

    demo commit

--- app/controllers/User.java
+++ app/controllers/User.java
@@ -12,4 +12,13 @@ public class User extends Controller {
     public static Result index(){
         return ok(index.render("ok! let go"));
     }
+
+    public static Result register(){
+        return ok(index.render("ok, insert"));
+    }
+
+    public static  Result update(){
+        return ok(index.render("ok, update 222"));
+    }
+
 }
